0e7037adfb0c08b1d7010458446ec39797ccadef
rafzal2020 <rafzal2014@gmail.com>
2026-03-31 23:21:13 -0400

    update mysh.c

--- mysh.c
+++ mysh.c
@@ -5,14 +5,38 @@
 
 #define BUF_SIZE 1024
 
+void execute_command(char *buf) {
+	if (buf[0] == '\0' || buf[0] == '#') {
+		return;	// empty line or comment
+	}
+	else if (strcmp(buf, "exit") == 0) {
+		write(STDOUT_FILENO, "Exiting my shell.\n", 18);
+		exit(EXIT_SUCCESS);
+	} else {
+		write(STDOUT_FILENO, "Command not yet implemented.\n", 28);
+	}
+}
+
 void input_loop(int interactive) {
 	char buf[BUF_SIZE];
+	char cwd[BUF_SIZE]; // working directory
+	char *home = getenv("HOME"); // '~'
 	int pos;
 	char c;
 
+	getcwd(cwd, sizeof(cwd));
+
 	while (1) {
-		if (interactive)
-		       write(STDOUT_FILENO, "$ ", 2);
+		if (interactive) {
+
+			if (home != NULL && strncmp(cwd, home, strlen(home)) == 0) {
+				write(STDOUT_FILENO, "~", 1);
+				write(STDOUT_FILENO, cwd + strlen(home), strlen(cwd + strlen(home)));
+			} else {
+				write (STDOUT_FILENO, cwd, strlen(cwd));
+			}
+			write(STDOUT_FILENO, "$ ", 2);
+		}
 
 		pos = 0;
 		while (1) {
@@ -24,8 +48,7 @@ void input_loop(int interactive) {
 			}
 
 			if (n == 0) {
-				if (interactive)
-					write (STDOUT_FILENO, "Exiting my shell.\n", 18);
+				write(STDOUT_FILENO, "\nExiting my shell.\n", 19);
 				return;
 			}
 
@@ -33,9 +56,28 @@ void input_loop(int interactive) {
 				buf[pos] = '\0';
 				break;
 			}
+
+			buf[pos++] = c;
+			if (pos >= BUF_SIZE - 1) {
+				buf[pos] = '\0';
+				break;
+			}
+		}
+
+		// executing commands
+		if (buf[0] == '\0' || buf[0] == '#') {
+			continue;	// empty line or comment
+		}
+		else if (strcmp(buf, "exit") == 0) {
+			write(STDOUT_FILENO, "Exiting my shell.\n", 18);
+			exit(EXIT_SUCCESS);
+		} else {
+			execute_command(buf);
+			write(STDOUT_FILENO, "\n", 1);
 		}
 	}
 }
+
 
 int main(void) {
 	int interactive = 1;
