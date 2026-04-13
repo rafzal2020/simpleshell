5c666beeae5ab3421ad6a65413ce1e80c10f9ebd
Anthony R. Rahner <arr234@ilab.cs.rutgers.edu>
2026-04-13 16:49:47 -0400

    Redirections work, execute... simplified with helpers

--- mysh.c
+++ mysh.c
@@ -24,48 +24,268 @@ int find_path(char *name, char *result)
     return 0;
 }
 
-int execute_command(char *buf, int interactive)
+// remove everything after # character
+void strip_comment(char *buf)
 {
-    (void)interactive; // to be used in the future
-
-    if (buf[0] == '\0')
-        return 0;
-
-    // strip comment starting at first #
     char *comment = strchr(buf, '#');
     if (comment != NULL)
     {
         *comment = '\0';
     }
+}
 
-    // tokenize
-    char *tokens[MAX_TOKENS];
+// separate command line by whitespace
+int tokenize_input(char *buf, char *tokens[])
+{
     int num_tokens = 0;
+
+    // strtok modifies buf by inserting '\0' terminators
     char *token = strtok(buf, " \t");
     while (token != NULL && num_tokens < MAX_TOKENS - 1)
     {
         tokens[num_tokens++] = token;
         token = strtok(NULL, " \t");
     }
+
+    // argument array ends with NULL
     tokens[num_tokens] = NULL;
+    return num_tokens;
+}
+
+// parse token list
+// separate <, > from actual command arguments
+int parse_redirection(char *tokens[], int num_tokens,
+                      char *args_cleaned[],
+                      char **in_file, char **out_file)
+{
+    int num_args = 0;
+
+    // default = no redirection
+    *in_file = NULL;
+    *out_file = NULL;
+
+    for (int i = 0; i < num_tokens; i++)
+    {
+        // input redirection
+        if (strcmp(tokens[i], "<") == 0)
+        {
+            // error if no filename after <
+            if (i + 1 >= num_tokens)
+                return -1;
+
+            // store input file, skip over it
+            *in_file = tokens[++i];
+        }
+
+        // same for output redirection
+        else if (strcmp(tokens[i], ">") == 0)
+        {
+            if (i + 1 >= num_tokens)
+                return -1;
+
+            *out_file = tokens[++i];
+        }
+        else
+        {
+            // add actual arguments to args_cleaned
+            args_cleaned[num_args++] = tokens[i];
+        }
+    }
+
+    // make sure args_cleaned ends with NULL
+    args_cleaned[num_args] = NULL;
+
+    // error if there are no actual commands
+    if (num_args == 0)
+        return -1;
+
+    return num_args;
+}
+
+// find executable path
+// if command contains /, treat it as a path
+// otherwise use find_path
+int resolve_command_path(char *cmd, char *path)
+{
+    if (strchr(cmd, '/') != NULL)
+    {
+        strncpy(path, cmd, BUF_SIZE - 1);
+        path[BUF_SIZE - 1] = '\0';
+        return 1;
+    }
+
+    return find_path(cmd, path);
+}
+
+// in child process, apply requested redirection
+int apply_child_redirection(char *in, char *out)
+{
+    if (in != NULL)
+    {
+        int fd = open(in, O_RDONLY);
+        if (fd < 0)
+        {
+            perror(in);
+            return 0;
+        }
+
+        // replace standard input (fd 0) with this file
+        if (dup2(fd, STDIN_FILENO) < 0)
+        {
+            perror("dup2");
+            close(fd);
+            return 0;
+        }
+
+        close(fd);
+    }
+
+    if (out != NULL)
+    {
+        int fd = open(out, O_WRONLY | O_CREAT | O_TRUNC, 0640);
+        if (fd < 0)
+        {
+            perror(out);
+            return 0;
+        }
+
+        // replace standard output (fd 1) with this file
+        if (dup2(fd, STDOUT_FILENO) < 0)
+        {
+            perror("dup2");
+            close(fd);
+            return 0;
+        }
+
+        close(fd);
+    }
+
+    return 1;
+}
+
+// report if child failed in interactive mode
+void report_child_status(int status, int interactive)
+{
+    if (!interactive)
+        return;
+
+    if (WIFEXITED(status))
+    {
+        int exit_code = WEXITSTATUS(status);
+        if (exit_code != 0)
+        {
+            char msg[BUF_SIZE];
+            int len = snprintf(msg, sizeof(msg),
+                               "Exited with status %d\n", exit_code);
+            write(STDOUT_FILENO, msg, len);
+        }
+    }
+    else if (WIFSIGNALED(status))
+    {
+        char msg[BUF_SIZE];
+        int sig = WTERMSIG(status);
+        int len = snprintf(msg, sizeof(msg),
+                           "Terminated by signal %d\n", sig);
+        write(STDOUT_FILENO, msg, len);
+    }
+}
+
+// run external command
+int external_command(char *args_cleaned[], char *in, char *out, int interactive)
+{
+    char path[BUF_SIZE];
+
+    // find executable file to be run
+    if (!resolve_command_path(args_cleaned[0], path))
+    {
+        write(STDERR_FILENO, "Command not found\n", 18);
+        return 0;
+    }
+
+    pid_t pid = fork();
+    if (pid < 0)
+    {
+        perror("fork");
+        return 0;
+    }
+
+    // child process
+    if (pid == 0)
+    {
+        // set up requested redirection
+        if (!apply_child_redirection(in, out))
+            exit(EXIT_FAILURE);
+
+        // replace child process with target program
+        execv(path, args_cleaned);
+
+        // only reached if execv fails
+        perror("execv");
+        exit(EXIT_FAILURE);
+    }
+    else
+    // parent process
+    {
+        int status;
+        if (waitpid(pid, &status, 0) < 0)
+        {
+            perror("waitpid");
+            return 0;
+        }
+
+        report_child_status(status, interactive);
+    }
+
+    return 0;
+}
+
+// main execution function
+int execute_command(char *buf, int interactive)
+{
+    (void)interactive;
+
+    // run through parsing helper functions
+    strip_comment(buf);
+
+    if (buf[0] == '\0')
+        return 0;
+
+    char *tokens[MAX_TOKENS];
+    int num_tokens = tokenize_input(buf, tokens);
 
     if (num_tokens == 0)
         return 0;
 
+    char *args_cleaned[MAX_TOKENS];
+    char *in = NULL;
+    char *out = NULL;
+    int num_args = parse_redirection(tokens, num_tokens,
+                                     args_cleaned, &in, &out);
+
+    if (num_args < 0)
+    {
+        write(STDERR_FILENO, "Syntax error\n", 13);
+        return 0;
+    }
+
     // built-in: exit
-    if (strcmp(tokens[0], "exit") == 0)
+    if (strcmp(args_cleaned[0], "exit") == 0)
     {
         return 1;
     }
 
     // built-in: cd
-    else if (strcmp(tokens[0], "cd") == 0)
+    else if (strcmp(args_cleaned[0], "cd") == 0)
     {
         char *dir;
-        if (num_tokens == 1)
+
+        // cd with no args goes to HOME
+        if (num_args == 1)
             dir = getenv("HOME");
-        else if (num_tokens == 2)
-            dir = tokens[1];
+
+        // otherwise transfer to given directory
+        else if (num_args == 2)
+            dir = args_cleaned[1];
         else
         {
             write(STDOUT_FILENO, "cd: too many arguments\n", 23);
@@ -87,61 +307,144 @@ int execute_command(char *buf, int interactive)
     }
 
     // built-in: pwd
-    else if (strcmp(tokens[0], "pwd") == 0)
+    else if (strcmp(args_cleaned[0], "pwd") == 0)
     {
+        int saved_stdout = -1;
+        int fd = -1;
+
+        // if output redirection was requested
+        // temporarily redirect stdout
+        if (out != NULL)
+        {
+            fd = open(out, O_WRONLY | O_CREAT | O_TRUNC, 0640);
+            if (fd < 0)
+            {
+                perror(out);
+                return 0;
+            }
+
+            // save original stdout for later
+            saved_stdout = dup(STDOUT_FILENO);
+            if (saved_stdout < 0)
+            {
+                perror("dup");
+                close(fd);
+                return 0;
+            }
+
+            // send stdout to output file
+            if (dup2(fd, STDOUT_FILENO) < 0)
+            {
+                perror("dup2");
+                close(fd);
+                close(saved_stdout);
+                return 0;
+            }
+
+            close(fd);
+        }
+
+        // print current working directory
         char cwd[BUF_SIZE];
         if (getcwd(cwd, sizeof(cwd)) == NULL)
         {
             perror("pwd");
-            return 0;
         }
-        write(STDOUT_FILENO, cwd, strlen(cwd));
-        write(STDOUT_FILENO, "\n", 1);
+        else
+        {
+            write(STDOUT_FILENO, cwd, strlen(cwd));
+            write(STDOUT_FILENO, "\n", 1);
+        }
+
+        // restore normal stdout
+        if (saved_stdout != -1)
+        {
+            dup2(saved_stdout, STDOUT_FILENO);
+            close(saved_stdout);
+        }
+
+        return 0;
+    }
+
+    // built-in: which
+    else if (strcmp(args_cleaned[0], "which") == 0)
+    {
+        int saved_stdout = -1;
+        int fd = -1;
+        char path[BUF_SIZE];
+
+        // if output redirection was requested
+        // temporarily redirect stdout
+        if (out != NULL)
+        {
+            fd = open(out, O_WRONLY | O_CREAT | O_TRUNC, 0640);
+            if (fd < 0)
+            {
+                perror(out);
+                return 0;
+            }
+
+            saved_stdout = dup(STDOUT_FILENO);
+            if (saved_stdout < 0)
+            {
+                perror("dup");
+                close(fd);
+                return 0;
+            }
+
+            if (dup2(fd, STDOUT_FILENO) < 0)
+            {
+                perror("dup2");
+                close(fd);
+                close(saved_stdout);
+                return 0;
+            }
+
+            close(fd);
+        }
+
+        // which takes exactly 1 argument
+        if (num_args != 2)
+        {
+            // fail
+        }
+        else if (strcmp(args_cleaned[1], "cd") == 0 ||
+                 strcmp(args_cleaned[1], "pwd") == 0 ||
+                 strcmp(args_cleaned[1], "which") == 0 ||
+                 strcmp(args_cleaned[1], "exit") == 0)
+        {
+            // built-ins do nothing with which
+        }
+
+        // treat argument as path with /
+        else if (strchr(args_cleaned[1], '/') != NULL)
+        {
+            if (access(args_cleaned[1], X_OK) == 0)
+            {
+                write(STDOUT_FILENO, args_cleaned[1], strlen(args_cleaned[1]));
+                write(STDOUT_FILENO, "\n", 1);
+            }
+        }
+
+        // otherwise search directories
+        else if (find_path(args_cleaned[1], path))
+        {
+            write(STDOUT_FILENO, path, strlen(path));
+            write(STDOUT_FILENO, "\n", 1);
+        }
+
+        // restore stdout if it was redirected
+        if (saved_stdout != -1)
+        {
+            dup2(saved_stdout, STDOUT_FILENO);
+            close(saved_stdout);
+        }
+
         return 0;
     }
 
     // external command: fork + execv
-    else
-    {
-        char path[BUF_SIZE];
-
-        // if it contains '/', use it directly, otherwise search
-        if (strchr(tokens[0], '/') != NULL)
-        {
-            strncpy(path, tokens[0], BUF_SIZE);
-            path[BUF_SIZE - 1] = '\0';
-        }
-        else if (!find_path(tokens[0], path))
-        {
-            write(STDOUT_FILENO, "Command not found\n", 18);
-            return 0;
-        }
-
-        pid_t pid = fork();
-        if (pid < 0)
-        {
-            perror("fork");
-            return 0;
-        }
-
-        if (pid == 0)
-        {
-            // child: execute the program
-            execv(path, tokens);
-            perror("execv");
-            exit(EXIT_FAILURE);
-        }
-        else
-        {
-            // parent: wait for child to finish
-            int status;
-            if (waitpid(pid, &status, 0) < 0)
-            {
-                perror("waitpid");
-            }
-        }
-    }
-    return 0;
+    return external_command(args_cleaned, in, out, interactive);
 }
 
 void input_loop(int input_fd, int interactive)
